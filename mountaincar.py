cfeb0d5b52f2df9a86fd3b50c64a1af948924839
Ricardo Carrillo <rcatorr93@gmail.com>
2023-01-16 03:58:14 +0900

    Setting up enviroments

---
+++ mountaincar.py
@@ -0,0 +1,17 @@
+import gym
+
+#Setting MountainCar-v0 as the environment
+env = gym.make("MountainCar-v0", render_mode="rgb_array")
+
+#Sets an initial state
+env.reset()
+
+# Rendering our instance 300 times
+for _ in range(300):
+    #Takes a random action from its action space 
+    # aka the number of unique actions an agent can perform
+    env.step(env.action_space.sample())
+    #renders the environment
+    env.render()
+
+env.close()
