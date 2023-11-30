2df1b1d7df3d26d9f68ee8c74949adefd4d43904
Ricardo Carrillo <rcatorr93@gmail.com>
2023-11-30 14:40:14 +0900

    Added Agent class and fixed visualization.

--- evo_island_main.py
+++ evo_island_main.py
@@ -6,31 +6,55 @@ import imageio
 
 level_difficulty = {1: 10, 2: 40, 3: 50, 4: 70, 5: 90}
 
-def visualize_matrix(matrix):
+class Agent:
+    def __init__(self, fitness=0, alive=False):
+        self.fitness = fitness
+        self.alive = alive if fitness > 0 else False
+
+    def is_more_fit_than(self, other):
+        return self.fitness > other.fitness
+
+
+#Matrix Creation, Visualization, Saving
+def visualize_world(matrix):
     fig, ax = plt.subplots()
     im = ax.imshow(matrix, cmap='viridis')
     plt.show()
+
+def visualize_agent_matrix(matrix):
+    fig, ax = plt.subplots()
+    im = ax.imshow([[cell.fitness for cell in row] for row in matrix], cmap='inferno')
+    plt.show() 
+
+def transform_matrix(agent_matrix, attribute):
+    return [[getattr(agent, attribute, 0) for agent in row] for row in agent_matrix]
 
 def save_matrix_image(matrix, file_name):
    fig, ax = plt.subplots()
    im = ax.imshow(matrix, cmap='viridis')
    plt.savefig(str(file_name))
+
+def save_agent_matrix_image(matrix, file_name, attribute):
+    # Check if the attribute is valid for the objects in the matrix
+    if not matrix or not hasattr(matrix[0][0], attribute):
+        raise ValueError(f"Attribute '{attribute}' not found in matrix elements")
+
+    # Transform the matrix into a 2D array of the specified attribute
+    transformed_matrix = [[getattr(cell, attribute) for cell in row] for row in matrix]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(transformed_matrix, cmap='viridis')
+    plt.colorbar(im, ax=ax)  # Optional: Adds a colorbar to the plot
+    plt.savefig(str(file_name))
+    plt.close(fig)
    
 
 def print_matrix(array_2d):
     for row in array_2d:
-        print(' '.join(str(cell) for cell in row))
+        print(' '.join(str(cell.fitness) for cell in row))
 
-def generate_agent_matrix(n):#Initialize empty agent matrix
- 
-    return [[0 for j in range(n)] for i in range(n)]
 
-def generate_island(n, use_perlin_noise=True, randomize_params=True, use_migration_route=True):
-    world = generate_terrain(n, use_perlin_noise, randomize_params)
-    if use_migration_route:
-       world = generate_river(n, world)
-
-    return world
+  #Terrain Generation
 
 def generate_petri_dish(n):
     if n <= 0:
@@ -115,6 +139,12 @@ def generate_river(n, world):
     # Return the world array
     return world
 
+def generate_island(n, use_perlin_noise=True, randomize_params=True, use_migration_route=True):
+    world = generate_terrain(n, use_perlin_noise, randomize_params)
+    if use_migration_route:
+       world = generate_river(n, world)
+
+    return world
 
 def isCoordinateInRange(n, x, y):
     if x < 0 or x >= n:
@@ -123,40 +153,44 @@ def isCoordinateInRange(n, x, y):
         return False
     return True
 
-def isMoreFit(challenger, Occupant):
-    return challenger > Occupant
-    
+#Agent Logic
+def isMoreFit(challenger, occupant):
+    return challenger.is_more_fit_than(occupant)
+
+def generate_agent_matrix(n):
+    return [[Agent() for j in range(n)] for i in range(n)]
+
 def train_new_individual():
     # generate a random number on a normal distribution
-    x = np.random.normal(loc=50, scale=10)
+    fitness = np.random.normal(loc=50, scale=10)
     # scale the number so that it falls within the range of 0 to 100
-    x = max(0, min(100, x))
+    fitness = max(0, min(100, fitness))
     # invert the number so that larger numbers are more rare
-    x = 100 - x
-    return int(x)
+    fitness = 100 - fitness
+    return Agent(int(fitness))
 
 def calc_move(i,j,n, world_matrix, agent_matrix):
    
-   if agent_matrix[i][j]== 0: # check if current cell is populated (max fitness is > 0), if not, do nothing. 
-    return 
-
-   #if occupied, the occupant spawns an offspring
+   current_agent = agent_matrix[i][j]
+   if current_agent.fitness == 0: return # Check if current cell is populated. if is not, skip. Add living or dead bool and update. 
+   
    newIndividual = train_new_individual()
+
 
    diceroll = random.randint(1, 9) #roll for which action the child will take
    #the offspring can move to an adacent valid cell, or challenge the parent for its niche
 
     #Stay at current cell and challenge parent
    if diceroll == 1:  
-    if newIndividual > level_difficulty[world_matrix[i][j]]: #Does the new invidual meet the baseline fitness to occupy the cell?
-        if isMoreFit(newIndividual,agent_matrix[i][j]): #Is the new individual's fitness higher than its parent? 
+    if newIndividual.fitness > level_difficulty[world_matrix[i][j]]: #Does the new invidual meet the baseline fitness to occupy the cell?
+        if newIndividual.is_more_fit_than(current_agent): #Is the new individual's fitness higher than its parent? 
             agent_matrix[i][j] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies. 
 
     #go north
    if diceroll == 2: 
        if isCoordinateInRange(n,i+1,j): #Is the move in range? 
-          if newIndividual > level_difficulty[world_matrix[i+1][j]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+          if newIndividual.fitness > level_difficulty[world_matrix[i+1][j]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i+1][j]): #Does the new individual's fitness beat the fitness of the current occupant?
             agent_matrix[i+1][j] = newIndividual #new individual takes the niche. 
             #or nothing happens and it dies. 
@@ -164,7 +198,7 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
     #go south
    if diceroll == 3: 
        if isCoordinateInRange(n,i-1,j): #Is the move in range?
-        if newIndividual > level_difficulty[world_matrix[i-1][j]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+        if newIndividual.fitness > level_difficulty[world_matrix[i-1][j]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i-1][j]):
             agent_matrix[i-1][j] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies. 
@@ -172,7 +206,7 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
     #go east
    if diceroll == 4:  
        if isCoordinateInRange(n,i,j+1): #Is the move in range?
-        if newIndividual > level_difficulty[world_matrix[i][j+1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+        if newIndividual.fitness > level_difficulty[world_matrix[i][j+1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i][j+1]):
             agent_matrix[i][j+1] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies. 
@@ -180,7 +214,7 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
     #go west
    if diceroll == 5:  
        if isCoordinateInRange(n,i,j-1): #Is the move in range?
-        if newIndividual > level_difficulty[world_matrix[i][j-1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+        if newIndividual.fitness > level_difficulty[world_matrix[i][j-1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i][j-1]):
             agent_matrix[i][j-1] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies. 
@@ -188,7 +222,7 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
     #go northeast
    if diceroll == 6:  
        if isCoordinateInRange(n,i+1,j+1): #Is the move in range?
-        if newIndividual > level_difficulty[world_matrix[i+1][j+1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+        if newIndividual.fitness > level_difficulty[world_matrix[i+1][j+1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i+1][j+1]):
             agent_matrix[i+1][j+1] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies. 
@@ -196,7 +230,7 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
     #go northwest
    if diceroll == 7:  
        if isCoordinateInRange(n,i+1,j-1): #Is the move in range?
-        if newIndividual > level_difficulty[world_matrix[i+1][j-1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+        if newIndividual.fitness > level_difficulty[world_matrix[i+1][j-1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i+1][j-1]):
             agent_matrix[i+1][j-1] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies. 
@@ -204,7 +238,7 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
     #go southeast
    if diceroll == 8:  
        if isCoordinateInRange(n,i-1,j+1): #Is the move in range?
-        if newIndividual > level_difficulty[world_matrix[i-1][j+1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+        if newIndividual.fitness > level_difficulty[world_matrix[i-1][j+1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i-1][j+1]):
             agent_matrix[i-1][j+1] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies. 
@@ -212,7 +246,7 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
     #go southwest
    if diceroll == 9:  
        if isCoordinateInRange(n,i-1,j-1): #Is the move in range?
-        if newIndividual > level_difficulty[world_matrix[i-1][j-1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
+        if newIndividual.fitness > level_difficulty[world_matrix[i-1][j-1]]: #Does the new invidual meet the baseline fitness to occupy the empty cell?
            if isMoreFit(newIndividual,agent_matrix[i-1][j-1]):
             agent_matrix[i-1][j-1] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies.       
@@ -222,16 +256,16 @@ def run_game():
 
     
     # configurable parameters, change as needed
-    map_size = 200
-    simulation_steps = 500
+    map_size = 500
+    simulation_steps = 1000
     use_perlin_noise = True  # set to False to use random integer generation instead
     use_random_params = False # set to False to use preset perlin parameters
     use_rivers = True # set to False to remove rivers
 
     #game initialization
     agent_starting_pos = 0,0
-    #game_world = generate_island (map_size, use_perlin_noise, use_random_params, use_rivers)
-    game_world = generate_petri_dish(map_size)
+    game_world = generate_island (map_size, use_perlin_noise, use_random_params, use_rivers)
+    #game_world = generate_petri_dish(map_size)
     agent_matrix = generate_agent_matrix(map_size)
 
     #place initial agent
@@ -248,7 +282,7 @@ def run_game():
     print ("Simulation Steps:" + str(simulation_steps))
     print ("\n")
     #print ("Displaying Game_World in console.")
-    visualize_matrix(game_world)
+    visualize_world(game_world)
     save_matrix_image(game_world, 'Game_World')
     
     frames = [] # array to save images of the heatmap
@@ -263,14 +297,27 @@ def run_game():
                 calc_move(i,j,map_size,game_world,agent_matrix)   
 
         current_sim_step += 1
-        im = plt.imshow(agent_matrix, cmap='viridis')
-        frames.append(im.get_array())
+        # Transform the agent_matrix into a numerical matrix for the 'fitness' attribute
+        fitness_matrix = transform_matrix(agent_matrix, 'fitness')
+
+        # Create the frame
+        fig, ax = plt.subplots()
+        im = ax.imshow(fitness_matrix, cmap='inferno')
+        ax.axis('off')  # Optional: Turn off the axis
+
+        # Draw canvas and convert to an image array
+        fig.canvas.draw()
+        image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+        image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+        frames.append(image)
+        plt.close(fig)
    
 
     #print final state
     #print ("Displaying Final_Map_State in console.")
     #visualize_matrix(agent_matrix)
-    save_matrix_image(agent_matrix, 'Final_Game_State')
+    save_agent_matrix_image(agent_matrix, 'Final_Game_State', 'fitness')
     
     #Create gif of execution
     print ("Generating gif...")
