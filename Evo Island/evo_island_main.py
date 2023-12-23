acbdab4b08303ab8196a2926504b191cba9939f0
Ricardo Carrillo <rcatorr93@gmail.com>
2023-12-24 05:07:15 +0900

    Implemented Inherited Mutation

--- Evo Island/evo_island_main.py
+++ Evo Island/evo_island_main.py
@@ -3,151 +3,18 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import imageio
+import json
+import os
 
-level_difficulty = {1: 10, 2: 40, 3: 50, 4: 70, 5: 90}
+#Config
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-class Agent:
-    def __init__(self, fitness=0, alive=False):
-        self.fitness = fitness
-        self.alive = alive
-        self.age = 0
-        self.maxage = 1000
+with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
 
-    def is_more_fit_than(self, other):
-        return self.fitness > other.fitness
+level_difficulty = {1: 1, 2: 20, 3: 50, 4: 70, 5: 90}
 
-
-#Matrix Creation, Visualization, Saving
-def visualize_world(matrix):
-    fig, ax = plt.subplots()
-    im = ax.imshow(matrix, cmap='viridis')
-    plt.show()
-
-def visualize_agent_attribute(matrix, attribute):
-   fig, ax = plt.subplots()
-   im = ax.imshow([[cell.attribute for cell in row] for row in matrix], cmap='viridis')
-   plt.show() 
-
-def transform_matrix(agent_matrix, attribute):
-    return [[getattr(agent, attribute, 0) for agent in row] for row in agent_matrix]
-
-def save_matrix_image(matrix, file_name):
-   fig, ax = plt.subplots()
-   im = ax.imshow(matrix, cmap='viridis')
-   plt.savefig(str(file_name))
-
-def save_agent_matrix_image(matrix, file_name, attribute):
-    # Check if the attribute is valid for the objects in the matrix
-    if not matrix or not hasattr(matrix[0][0], attribute):
-        raise ValueError(f"Attribute '{attribute}' not found in matrix elements")
-
-    # Transform the matrix into a 2D array of the specified attribute
-    transformed_matrix = [[getattr(cell, attribute) for cell in row] for row in matrix]
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(transformed_matrix, cmap='viridis')
-    plt.colorbar(im, ax=ax)  # Optional: Adds a colorbar to the plot
-    plt.savefig(str(file_name))
-    plt.close(fig)
-   
-
-def print_matrix(array_2d):
-    for row in array_2d:
-        print(' '.join(str(cell.fitness) for cell in row))
-
-
-  #Terrain Generation
-
-def generate_petri_dish(n):
-    if n <= 0:
-        return []
-
-    array = [[0] * n for _ in range(n)]
-    increment = 4 / (n - 1)
-
-    for i in range(n):
-        for j in range(n):
-            array[i][j] = round((j * increment) + 1)
-
-    return array
-    
-def generate_terrain(n, use_perlin_noise=True, randomize_params=True):
-    if use_perlin_noise:
-        if randomize_params:
-            scale = random.uniform(50, 200)
-            octaves = random.randint(4, 8)
-            persistence = random.uniform(0.4, 0.7)
-            lacunarity = random.uniform(1.8, 2.2)
-        else:
-            scale = 100.0  # adjust this parameter to change the "roughness" of the terrain
-            octaves = 6    # adjust this parameter to change the level of detail in the terrain
-            persistence = 0.5  # adjust this parameter to change the balance of high and low terrain
-            lacunarity = 2.0    # adjust this parameter to change the level of detail in the terrain
-
-        # Generate Perlin noise values for each cell in the array
-        world = np.zeros((n, n))
-        for i in range(n):
-            for j in range(n):
-                world[i][j] = noise.pnoise2(i/scale, j/scale, octaves=octaves, persistence=persistence, 
-                                            lacunarity=lacunarity, repeatx=n, repeaty=n, base=0)
-
-        # Scale and shift the values to the range [1, 5]
-        world = (world - np.min(world)) * (5-1)/(np.max(world)-np.min(world)) + 1
-
-        # Round the values to integers
-        world = np.round(world)
-
-        # Set the top left cell to biome 1, starting cell is always a "safe" zone. 
-        world[0][0] = 1
-
-        return world.astype(int).tolist()
-    
-    else:
-        #Map is randomly generated with no structure. 
-        return [[random.randint(1, 3) for i in range(n)] for j in range(n)]
-
-from noise import pnoise2
-
-def generate_river(n, world):
-    # Generate 2D Perlin noise
-    scale = 200.0
-    octaves = 6
-    persistence = 0.4
-    lacunarity = 2.0
-    grid = [[0 for j in range(n)] for i in range(n)]
-    for i in range(n):
-        for j in range(n):
-            grid[i][j] = pnoise2(i / scale, j / scale, octaves=octaves,
-                                 persistence=persistence, lacunarity=lacunarity,
-                                 repeatx=n, repeaty=n, base=0)
-
-    # Find the maximum and minimum values in the grid
-    max_val = max(map(max, grid))
-    min_val = min(map(min, grid))
-
-    # Rescale the values in the grid to be between 0 and 1
-    scaled_grid = [[(val - min_val) / (max_val - min_val) for val in row] for row in grid]
-
-    # Define the river path as a series of points with y-values along the center of the grid
-    river_width = n // 50
-    river_path = [[i, scaled_grid[i][n//2]] for i in range(n)]
-
-    # Write the river pattern directly to the world array
-    for i in range(n):
-        for j in range(n):
-            if abs(int(river_path[i][1]*n) - j) <= river_width // 2:
-                world[i][j] = 1
-
-    # Return the world array
-    return world
-
-def generate_island(n, use_perlin_noise=True, randomize_params=True, use_migration_route=True):
-    world = generate_terrain(n, use_perlin_noise, randomize_params)
-    if use_migration_route:
-       world = generate_river(n, world)
-
-    return world
-
+#Helper Methods
 def isCoordinateInRange(n, x, y):
     if x < 0 or x >= n:
         return False
@@ -155,6 +22,25 @@ def isCoordinateInRange(n, x, y):
         return False
     return True
 
+def find_easiest_difficulty_location(world_matrix):
+    for i, row in enumerate(world_matrix):
+        for j, difficulty in enumerate(row):
+            if difficulty == 1:
+                return (i, j)  # Return the coordinates as a tuple (row, column)
+    return None  # Return None if no cell with that difficulty is found
+
+
+#Agent Class
+class Agent:
+    def __init__(self, fitness=0, alive=False):
+        self.fitness = fitness
+        self.alive = alive
+        self.age = 0
+        self.maxage = 100
+
+    def is_more_fit_than(self, other):
+        return self.fitness > other.fitness
+    
 #Agent Logic
 def isMoreFit(challenger, occupant):
     return challenger.is_more_fit_than(occupant)
@@ -171,6 +57,16 @@ def generate_new_individual():
     fitness = 100 - fitness
     return Agent(int(fitness), alive=True)
 
+def generate_new_individual_from_parent(parent_agent):
+    mutation_probability = 0.00084 #True mutation rate of E. Coli.
+    mutation_effect = np.random.normal(loc=0, scale=2) if random.random() < mutation_probability else 0
+    fitness = parent_agent.fitness + mutation_effect
+    fitness = max(0, min(100, fitness))
+
+    # Implement fitness cost or reproduction threshold if needed
+    # ...
+
+    return Agent(int(fitness), alive=True)
 
 def calc_move(i,j,n, world_matrix, agent_matrix):
    
@@ -182,8 +78,8 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
       agent_matrix[i][j] = Agent()
       return 
 
-   newIndividual = generate_new_individual()
-
+   #newIndividual = generate_new_individual()
+   newIndividual = generate_new_individual_from_parent(current_agent)
 
    diceroll = random.randint(1, 9) #roll for which action the child will take
    #the offspring can move to an adacent valid cell, or challenge the parent for the current cell
@@ -259,26 +155,153 @@ def calc_move(i,j,n, world_matrix, agent_matrix):
             agent_matrix[i-1][j-1] = newIndividual #new individual takes the niche.
             #or nothing happens and it dies.       
 
+
+  #Terrain Generation
+
+def generate_petri_dish(n):
+    if n <= 0:
+        return []
+
+    array = [[0] * n for _ in range(n)]
+    increment = 4 / (n - 1)
+
+    for i in range(n):
+        for j in range(n):
+            array[i][j] = round((j * increment) + 1)
+
+    return array
+    
+def generate_terrain(n, use_perlin_noise=True, randomize_params=True):
+    if use_perlin_noise:
+        if randomize_params:
+            scale = random.uniform(50, 200)
+            octaves = random.randint(4, 8)
+            persistence = random.uniform(0.4, 0.7)
+            lacunarity = random.uniform(1.8, 2.2)
+        else:
+            scale = 100.0  # adjust this parameter to change the "roughness" of the terrain
+            octaves = 6    # adjust this parameter to change the level of detail in the terrain
+            persistence = 0.5  # adjust this parameter to change the balance of high and low terrain
+            lacunarity = 2.0    # adjust this parameter to change the level of detail in the terrain
+
+        # Generate Perlin noise values for each cell in the array
+        world = np.zeros((n, n))
+        for i in range(n):
+            for j in range(n):
+                world[i][j] = noise.pnoise2(i/scale, j/scale, octaves=octaves, persistence=persistence, 
+                                            lacunarity=lacunarity, repeatx=n, repeaty=n, base=0)
+
+        # Scale and shift the values to the range [1, 5]
+        world = (world - np.min(world)) * (5-1)/(np.max(world)-np.min(world)) + 1
+
+        # Round the values to integers
+        world = np.round(world)
+
+        # Set the top left cell to biome 1, starting cell is always a "safe" zone. 
+        #world[0][0] = 1
+
+        return world.astype(int).tolist()
+    
+    else:
+        #Map is randomly generated with no structure. 
+        return [[random.randint(1, 3) for i in range(n)] for j in range(n)]
+
+from noise import pnoise2
+
+def generate_river(n, world):
+    # Generate 2D Perlin noise
+    scale = 200.0
+    octaves = 6
+    persistence = 0.4
+    lacunarity = 2.0
+    grid = [[0 for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = pnoise2(i / scale, j / scale, octaves=octaves,
+                                 persistence=persistence, lacunarity=lacunarity,
+                                 repeatx=n, repeaty=n, base=0)
+
+    # Find the maximum and minimum values in the grid
+    max_val = max(map(max, grid))
+    min_val = min(map(min, grid))
+
+    # Rescale the values in the grid to be between 0 and 1
+    scaled_grid = [[(val - min_val) / (max_val - min_val) for val in row] for row in grid]
+
+    # Define the river path as a series of points with y-values along the center of the grid
+    river_width = n // 50
+    river_path = [[i, scaled_grid[i][n//2]] for i in range(n)]
+
+    # Write the river pattern directly to the world array
+    for i in range(n):
+        for j in range(n):
+            if abs(int(river_path[i][1]*n) - j) <= river_width // 2:
+                world[i][j] = 1
+
+    # Return the world array
+    return world
+
+def generate_island(n, use_perlin_noise=True, randomize_params=True, use_migration_route=True):
+    world = generate_terrain(n, use_perlin_noise, randomize_params)
+    if use_migration_route:
+       world = generate_river(n, world)
+
+    return world
+
+#Matrix Creation, Visualization, Saving
+def visualize_world(matrix):
+    fig, ax = plt.subplots()
+    im = ax.imshow(matrix, cmap='viridis')
+    plt.show()
+
+def visualize_agent_attribute(matrix, attribute):
+   fig, ax = plt.subplots()
+   im = ax.imshow([[cell.attribute for cell in row] for row in matrix], cmap='viridis')
+   plt.show() 
+
+def transform_matrix(agent_matrix, attribute):
+    return [[getattr(agent, attribute, 0) for agent in row] for row in agent_matrix]
+
+def save_matrix_image(matrix, file_name):
+   fig, ax = plt.subplots()
+   im = ax.imshow(matrix, cmap='viridis')
+   plt.savefig(str(file_name))
+
+def save_agent_matrix_image(matrix, file_name, attribute):
+    # Check if the attribute is valid for the objects in the matrix
+    if not matrix or not hasattr(matrix[0][0], attribute):
+        raise ValueError(f"Attribute '{attribute}' not found in matrix elements")
+
+    # Transform the matrix into a 2D array of the specified attribute
+    transformed_matrix = [[getattr(cell, attribute) for cell in row] for row in matrix]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(transformed_matrix, cmap='viridis')
+    plt.colorbar(im, ax=ax)  # Optional: Adds a colorbar to the plot
+    plt.savefig(str(file_name))
+    plt.close(fig)
+
 #main game loop
 def run_game():
 
-    
-    # configurable parameters, change as needed
-    map_size = 300
-    simulation_steps = 1000
-    use_perlin_noise = True  # set to False to use random integer generation instead
-    use_random_params = False # set to False to use preset perlin parameters
-    use_rivers = True # set to False to remove rivers
-    frame_save_interval = 10 #Sets rate of frame saving
+    # Use the configurations
+    map_size = config['map_size']
+    simulation_steps = config['simulation_steps']
+    use_perlin_noise = config['use_perlin_noise']
+    use_random_params = config['use_random_params']
+    use_rivers = config['use_rivers']
+    frame_save_interval = config['frame_save_interval']
+    frame_rate = config['frame_rate']
 
-    #game initialization
-    agent_starting_pos = 0,0
+    #game world initialization
     game_world = generate_island (map_size, use_perlin_noise, use_random_params, use_rivers)
     #game_world = generate_petri_dish(map_size)
+    
+    #agent initialization
     agent_matrix = generate_agent_matrix(map_size)
-
-    #place initial agent
-    agent_matrix[agent_starting_pos[0]][agent_starting_pos[1]] = generate_new_individual()
+    #agent_starting_pos = 0,0
+    agent_starting_pos = find_easiest_difficulty_location(game_world)
+    agent_matrix[agent_starting_pos[0]][agent_starting_pos[1]] = Agent(fitness=10, alive = True) #Create initial
     #agent_matrix[random.randint(0, len(agent_matrix)-1)][random.randint(0, len(agent_matrix[0])-1)] = generate_new_individual()
 
 
@@ -321,7 +344,7 @@ def run_game():
 
              # Create frames for visualization
             fig1, ax1 = plt.subplots()
-            im1 = ax1.imshow(fitness_matrix, cmap='inferno')
+            im1 = ax1.imshow(fitness_matrix, cmap='viridis')
             ax1.axis('off')  # Optional: Turn off the axis
                     
             fig2, ax2 = plt.subplots()
@@ -351,8 +374,8 @@ def run_game():
     
     #Create gif of execution
     print ("Generating gifs...")
-    imageio.mimsave('fitnessmap.gif', fitness_frames, fps=15)
-    imageio.mimsave('agemap.gif', age_frames, fps=15)
+    imageio.mimsave('fitnessmap.gif', fitness_frames, fps=frame_rate)
+    imageio.mimsave('agemap.gif', age_frames, fps=frame_rate)
 
     print ("Simulation complete.") 
     print ("\n")
